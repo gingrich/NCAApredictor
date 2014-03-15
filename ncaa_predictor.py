150cc1ad7166e015c5a86f3f224033b24f5f3ad4
gingrich <mrkgingrich@gmail.com>
2014-03-15 12:27:31 -0700

    Updated ncaa_predictory.py
    Contains Linear SVC method, logloss cost function, and bulk testing
    method.

--- ncaa_predictor.py
+++ ncaa_predictor.py
@@ -2,8 +2,9 @@ from __future__ import division
 import pandas as pd
 from lookup_kaggle_to_kenpom import team_lookup as kag_to_kp
 from lookup_kenpom_to_kaggle import team_lookup as kp_to_kag
-import re,sys
+import re
 from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
 import numpy as np
 from nltk import edit_distance
 
@@ -66,7 +67,7 @@ class PredictNCAA():
             return name
         
     #Trains a Logistic Regression model_________________________________________                
-    def train_LR(self):
+    def training_testing_split(self):
         X = []
         y = []
         for year in range(2003,2013):
@@ -75,7 +76,7 @@ class PredictNCAA():
                     line = line.split(',')
                     team1 = line[0]
                     team2 = line[1]
-                    team1wins = line[2]
+                    team1wins = int(line[2].strip())
                     if team1 not in self.teams_kaggle.name.values:
                         team1 = self.get_teamname(team1)
                     team1id = self.teams_kaggle.id[self.teams_kaggle.name==team1].values[0]
@@ -89,14 +90,37 @@ class PredictNCAA():
                     game_feats = team1feats + team2feats
                     X.append(game_feats)
                     y.append(team1wins)
-        self.X = X
-        self.y = y
-                        
+        N = int(.7*len(X))
+        self.X_train = X[:N]
+        self.y_train = y[:N]
+        self.X_test = X[N:]
+        self.y_test = y[N:]
+           
+    def train_LR(self):             
         self.LR = LogisticRegression()
-        self.LR.fit(self.X,self.y)
+        self.LR.fit(self.X_train,self.y_train)
+        
+    def train_SVC(self):
+        self.SVC = SVC(probability=True)
+        self.SVC.fit(self.X_train,self.y_train)
+                
+    def test(self,classifier):
+        print classifier.score(self.X_test,self.y_test)
+        
+    def logloss(self,classifier):
+        pred = classifier.predict_proba(self.X_test)
+        s = 0
+        for p in range(len(pred)):
+            prob_one = pred[p][1]
+            prob_zero = pred[p][0]
+            truth = self.y_test[p]
+            s += (truth*np.log(prob_one) + (1-truth)*np.log(prob_zero))
+        print -s/len(self.y_test)
+            
+        
         
     #Predicts the probabilistic outcome of a single game________________________
-    def predict_game(self,team1name,team2name):
+    def predict_game(self,team1name,team2name,classifier):
         if team1name not in self.teams_kaggle.name.values:
             team1name = self.get_teamname(team1name)
         team1id = self.teams_kaggle.id[self.teams_kaggle.name==team1name].values[0]
@@ -109,17 +133,25 @@ class PredictNCAA():
         team2feats = [(float(self.all_data[year][team2id][x]) - self.means[x])/self.stdevs[x]
                             for x in self.feature_names]
         x = team1feats + team2feats
-        pred = self.LR.predict_proba(x)
+        pred = classifier.predict_proba(x)
         return pred
 
 if __name__ == '__main__':
-    team1 = sys.argv[1]
-    team2 = sys.argv[2]
+
     a = PredictNCAA()
     a.load_data()
     a.standardize_feats()
+    a.training_testing_split()
     a.train_LR()
-    pred = a.predict_game(team1,team2)[0]
+    a.train_SVC()
+    a.test(a.SVC)
+    a.test(a.LR)
+    a.logloss(a.SVC)
+    a.logloss(a.LR)
+        
+    team1 = 'Washington'
+    team2 = 'UCLA'
+    pred = a.predict_game(team1,team2,a.SVC)[0]
     print "Percent chances of winning:"
     print ("{team1}: %0.2f"%(pred[1]*100)).format(team1=team1)+"%"
     print ("{team2}: %0.2f"%(pred[0]*100)).format(team2=team2)+"%"
