ab0b28ff1e97b361f6eb9c07030bc14e2d1ab8b4
gingrich <mrkgingrich@gmail.com>
2014-03-18 18:43:14 -0700

    Added ensemble method
    Final submission

--- ncaa_predictor.py
+++ ncaa_predictor.py
@@ -163,6 +163,33 @@ class PredictNCAA():
     def test(self,classifier,X,y):
         print classifier.score(X,y)
         
+    def test_ensemble(self,X,y):
+        labels = a.RF.predict(X)
+        pred_SVC = a.SVC.predict_proba(X)
+        pred_LR = a.LR.predict_proba(X)
+        agree = []
+        pred = []
+        for i in range(len(labels)):
+            if pred_LR[i][labels[i]] > pred_SVC[i][labels[i]]:
+                pred.append(pred_LR[i][1])
+                if (pred_LR[i][1] < .5 and y[i]) or (pred_LR[i][1] > .5 and not y[i]):
+                    agree.append(0)
+                else: agree.append(1)
+            else:
+                pred.append(pred_SVC[i][1])
+                if (pred_SVC[i][1] < .5 and y[i]) or (pred_SVC[i][1] > .5 and not y[i]):
+                    agree.append(0)
+                else: agree.append(1)
+        print np.mean(agree) 
+        
+        s = 0
+        for p in range(len(pred)):
+            prob_one = pred[p]
+            truth = y[p]
+            s += (truth*np.log(prob_one) + (1-truth)*np.log(1-prob_one))
+        print -s/len(y)
+                          
+          
     def logloss(self,classifier,X,y):
         pred = classifier.predict_proba(X)
         s = 0
@@ -187,9 +214,34 @@ class PredictNCAA():
         team2feats = [(float(self.all_data[year][team2id][x]) - self.means[x])/self.stdevs[x]
                             for x in self.feature_names]
         game_feats = [team1feats[i] - team2feats[i] for i in range(len(team1feats))]
-        #game_feats = team1feats + team2feats
         pred = classifier.predict_proba(game_feats)
         return pred
+        
+    def predict_ensemble(self,team1name,team2name):
+        if team1name not in self.teams_kaggle.name.values:
+            team1name = self.get_teamname(team1name)
+        team1id = self.teams_kaggle.id[self.teams_kaggle.name==team1name].values[0]
+        if team2name not in self.teams_kaggle.name.values:
+            team2name = self.get_teamname(team2name)
+        team2id = self.teams_kaggle.id[self.teams_kaggle.name==team2name].values[0]
+        year=2014
+        team1feats = [(float(self.all_data[year][team1id][x]) - self.means[x])/self.stdevs[x] 
+                            for x in self.feature_names]
+        team2feats = [(float(self.all_data[year][team2id][x]) - self.means[x])/self.stdevs[x]
+                            for x in self.feature_names]
+        game_feats1 = [team1feats[i] - team2feats[i] for i in range(len(team1feats))]
+        pred1 = self.RF.predict(game_feats1)[0]
+        game_feats2 = [team2feats[i] - team1feats[i] for i in range(len(team1feats))]
+        pred2 = self.RF.predict(game_feats2)[0]
+        svcpred = self.SVC.predict_proba(game_feats1)
+        lrpred = self.LR.predict_proba(game_feats1)
+        if pred1 == pred2:
+            return lrpred
+        else:
+            if lrpred[0][pred1] > svcpred[0][pred1]:
+                return lrpred
+            else:
+                return svcpred
         
     def process_tournament(self,gameid,classifier):
         team1id = int(gameid.split('_')[1])
@@ -202,6 +254,28 @@ class PredictNCAA():
         game_feats = [team1feats[i] - team2feats[i] for i in range(len(team1feats))]
         pred = classifier.predict_proba(game_feats)
         return pred[0][1]
+        
+    def process_tournament_ensemble(self,gameid):
+        team1id = int(gameid.split('_')[1])
+        team2id = int(gameid.split('_')[-1])
+        year=2014
+        team1feats = [(float(self.all_data[year][team1id][x]) - self.means[x])/self.stdevs[x] 
+                    for x in self.feature_names]
+        team2feats = [(float(self.all_data[year][team2id][x]) - self.means[x])/self.stdevs[x]
+                            for x in self.feature_names]
+        game_feats1 = [team1feats[i] - team2feats[i] for i in range(len(team1feats))]
+        pred1 = self.RF.predict(game_feats1)[0]
+        svcpred = self.SVC.predict_proba(game_feats1)
+        lrpred = self.LR.predict_proba(game_feats1)
+        game_feats2 = [team2feats[i] - team1feats[i] for i in range(len(team1feats))]
+        pred2 = self.RF.predict(game_feats2)[0]
+        if pred1 == pred2:
+            return lrpred[0][1]
+        else:
+            if lrpred[0][pred1] > svcpred[0][pred1]:
+                return lrpred[0][1]
+            else:
+                return svcpred[0][1]
         
     def tune_SVC():
         C_range = 10.0 ** np.arange(-2, 9)
@@ -229,19 +303,29 @@ if __name__ == '__main__':
     a.load_blakeData()
     a.standardize_feats()
     a.training_testing_split()
-    a.train_LR(a.X_train,a.y_train)
-    a.train_SVC(a.X_train,a.y_train)
-    a.train_RF(a.X_train,a.y_train)
+    a.train_LR(a.X_full,a.y_full)
+    a.train_SVC(a.X_full,a.y_full)
+    a.train_RF(a.X_full,a.y_full)
+    
+    print "SVC Test"
     a.test(a.SVC,a.X_test,a.y_test)
-    a.test(a.LR,a.X_test,a.y_test)
-    a.test(a.RF,a.X_test,a.y_test)
     a.logloss(a.SVC,a.X_test,a.y_test)
+
+    print "LR Test"
+    a.test(a.LR,a.X_test,a.y_test)
     a.logloss(a.LR,a.X_test,a.y_test)
+
+    print "RF Test"
+    a.test(a.RF,a.X_test,a.y_test)
     a.logloss(a.RF,a.X_test,a.y_test)
+
+    print "Ensemble Test"
+    a.test_ensemble(a.X_test,a.y_test)
         
-    team1 = 'Wichita St'
-    team2 = 'Arizona'
-    pred = a.predict_game(team1,team2,a.RF)[0]
+    team1 = 'Albany'
+    team2 = 'Mt St Mary\'s'
+    #pred = a.predict_ensemble(team1,team2)[0]
+    pred = a.predict_game(team1,team2,a.SVC)[0]
     print "Percent chances of winning:"
     print ("{team1}: %0.2f"%(pred[1]*100)).format(team1=team1)+"%"
     print ("{team2}: %0.2f"%(pred[0]*100)).format(team2=team2)+"%"
@@ -251,3 +335,5 @@ if __name__ == '__main__':
     #submit.to_csv('submission_LR.csv',index=False)
     #submit.pred = submit.id.apply(lambda x: a.process_tournament(x,a.SVC))
     #submit.to_csv('submission_SVC.csv',index=False)
+    #submit.pred = submit.id.apply(lambda x: a.process_tournament_ensemble(x))
+    #submit.to_csv('submission_ensemble.csv',index=False)
